7ac63e047590670f1b14d04a3abf9f57caf84cfc
Binbin Fan <bf294508@hotmail.com>
2020-01-14 23:32:34 -0500

    finial

---
+++ client/build/precache-manifest.b68f752ba1ba3ef5235e086da88e80bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f56c2d142f113efd57f0",
+    "url": "/static/css/main.3fd27e83.chunk.css"
+  },
+  {
+    "revision": "f56c2d142f113efd57f0",
+    "url": "/static/js/main.e5f8ba76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fe0b80cb9f8d02f4ffc8",
+    "url": "/static/js/2.681f4ec5.chunk.js"
+  },
+  {
+    "revision": "599f08b147e67ddb276bf4cea6d1b33b",
+    "url": "/index.html"
+  }
+];
